b9ef153f8e28f250963324c1f10e70575a66e2e8
Gabriel Silva Hermida <silva.gabriel2207@gmail.com>
2021-02-07 10:45:08 -0300

    Dino game
    Signed-off-by: Gabriel Silva Hermida <silva.gabriel2207@gmail.com>

---
+++ script.js
@@ -0,0 +1,71 @@
+const dino = document.querySelector('.dino');
+
+const background = document.querySelector('.background')
+
+let position = 0;
+
+let isJumping = false;
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        if(!isJumping){
+            jump();
+        }
+    }
+}
+//pulo do dinossauro
+function jump() {
+    isJumping = true;
+    //subida
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+            clearInterval(upInterval)
+            //descida
+            let downInterval = setInterval(() => {
+                if(position <= 0){
+                    clearInterval(downInterval)
+                    isJumping = false;
+                }
+                else{
+                    position -= 20
+                    dino.style.bottom = position + 'px';    
+                }
+            }, 20)
+        }
+        else{
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20)
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    let randomTime = Math.random() * 4000;
+    console.log(randomTime)
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class = "game-over">Fim de jogo</h1>'
+        } else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';    
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime)
+}
+
+createCactus();
+
+document.addEventListener('keyup', handleKeyUp)
